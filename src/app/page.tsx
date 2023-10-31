6a4b1c6a5ec7321a5bb83b788d98abb9b41e94f2
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-10-31 10:38:21 -0300

    feat: fetch data from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <h1>Home</h1>;
-}
+import CharacterCard from "@/components/character-card";
+import { Character } from "@/types/character";
+
+const getData = async (): Promise<Character[]> => {
+  const res = await fetch(
+    "http://homologacao3.azapfy.com.br/api/ps/metahumans",
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+const Home = async () => {
+  const characters = await getData();
+
+  return (
+    <>
+      <ul className="grid w-1/2 grid-cols-5 gap-4">
+        {characters.map((character) => (
+          <li key={character.id}>
+            <CharacterCard character={character} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Home;
