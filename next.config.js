4eb20a22d0e7262c1ac8d162377bc50b5b4630e1
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-10-31 10:35:04 -0300

    chore: add next.js images config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
